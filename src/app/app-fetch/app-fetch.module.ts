67186e564e00ed63c8cc7e5684b2581be19a5e32
dkn1ght23 <mujammal.salman@gmail.com>
2022-07-08 15:18:57 +0600

    responsive update 1

--- src/app/app-fetch/app-fetch.module.ts
+++ src/app/app-fetch/app-fetch.module.ts
@@ -5,35 +5,43 @@ import { AppFetchRoutingModule } from './app-fetch-routing.module';
 import { FetchDefaultComponent } from './components/fetch-default/fetch-default.component';
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
-import {FlexModule} from "@angular/flex-layout";
+import {FlexLayoutModule, FlexModule} from "@angular/flex-layout";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import { FetchSearchComponent } from './components/fetch-search/fetch-search.component';
 import { FetchDetailsComponent } from './components/fetch-details/fetch-details.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatExpansionModule} from "@angular/material/expansion";
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import {CdkTableModule} from "@angular/cdk/table";
+import {MatMenuModule} from "@angular/material/menu";
 
 @NgModule({
     declarations: [
         FetchDefaultComponent,
         FetchSearchComponent,
-      FetchDetailsComponent
+      FetchDetailsComponent,
+      NavBarComponent
     ],
-  exports: [
-    FetchDefaultComponent,
-    FetchSearchComponent,
-    FetchDetailsComponent
-  ],
-    imports: [
-        CommonModule,
-        AppFetchRoutingModule,
-        MatInputModule,
-        FormsModule,
-        FlexModule,
-        MatButtonModule,
-        MatIconModule,
-        MatCardModule,
-        MatExpansionModule
-    ]
+    exports: [
+        FetchDefaultComponent,
+        FetchSearchComponent,
+        FetchDetailsComponent,
+        NavBarComponent
+    ],
+  imports: [
+    CommonModule,
+    AppFetchRoutingModule,
+    MatInputModule,
+    FormsModule,
+    FlexModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatExpansionModule,
+    FlexLayoutModule,
+    CdkTableModule,
+    MatMenuModule
+  ]
 })
 export class AppFetchModule { }
